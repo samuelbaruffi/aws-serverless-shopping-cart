5c2144059d55d5602aaa601e1476641e2033ad27
Baruffi <samu.lord@gmail.com>
2021-09-16 08:08:41 -0700

    Adding support for orders

--- backend/shopping-cart-service/checkout_cart.py
+++ backend/shopping-cart-service/checkout_cart.py
@@ -1,5 +1,6 @@
 import json
 import os
+import time
 
 import boto3
 from aws_lambda_powertools import Logger, Metrics, Tracer
@@ -15,6 +16,8 @@ dynamodb = boto3.resource("dynamodb")
 
 logger.debug("Initializing DDB Table %s", os.environ["TABLE_NAME"])
 table = dynamodb.Table(os.environ["TABLE_NAME"])
+logger.debug("Initializing DDB Table %s", os.environ["TABLE_NAME_ORDERS"])
+table_order = dynamodb.Table(os.environ["TABLE_NAME_ORDERS"])
 
 
 @metrics.log_metrics(capture_cold_start_metric=True)
@@ -42,13 +45,32 @@ def lambda_handler(event, context):
     response = table.query(
         KeyConditionExpression=Key("pk").eq(f"user#{user_id}")
         & Key("sk").begins_with("product#"),
-        ConsistentRead=True,  # Perform a strongly consistent read here to ensure we get correct and up to date cart
+        # Perform a strongly consistent read here to ensure we get correct and up to date cart
+        ConsistentRead=True,
     )
 
+    epoch = time.time()
     cart_items = response.get("Items")
     # batch_writer will be used to update status for cart entries belonging to the user
     with table.batch_writer() as batch:
+        print(cart_items)
         for item in cart_items:
+
+            # Add items to orders table
+            orders_pk = f"order#{user_id}#{epoch}"
+            product_sk = item["sk"]
+            orders_sk = f"product#{product_sk}"
+            # product_id = item["product_id"]
+            # quantity = item["quantity"]
+            table_order.put_item(
+                Item={
+                    "pk": orders_pk,
+                    "sk": orders_sk,
+                    "quantity": item["quantity"],
+                    "productDetail": item["productDetail"],
+                }
+            )
+
             # Delete ordered items
             batch.delete_item(Key={"pk": item["pk"], "sk": item["sk"]})
 
